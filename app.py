3fae37687cd902cceda8106ed007cf1105f5d0be
RafaelApfernandes97 <rafael.apfernandes78@gmail.com>
2024-06-12 09:45:08 -0300

    email.v3

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import threading
 import time
 import os
 from flask_paginate import Pagination, get_page_args
+from email_validator import validate_email, EmailNotValidError
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -25,19 +26,29 @@ subject = ""
 html_content = ""
 
 def load_recipients(file_path):
+    valid_emails = []
     try:
         df = pd.read_csv(file_path)
         if 'email' not in df.columns:
             raise KeyError('A coluna "email" não foi encontrada no arquivo CSV.')
-        return df['email'].tolist()
+        
+        for email in df['email']:
+            try:
+                # Valida o endereço de e-mail
+                valid = validate_email(email).email
+                valid_emails.append(valid)
+            except EmailNotValidError as e:
+                flash(f"E-mail inválido encontrado: {email} - {str(e)}", 'danger')
+
+        return valid_emails
     except Exception as e:
         flash(str(e), 'danger')
         return []
 
 def send_email(to_address, subject, html_content):
-    from_address = 'your_email@example.com'
-    password = 'your_password'
-    smtp_server = 'smtp.example.com'
+    from_address = 'suporte@mais.chat'
+    password = 'myuniodjrosioghc'
+    smtp_server = 'smtp.gmail.com'
     smtp_port = 587
 
     msg = MIMEMultipart('alternative')
@@ -84,8 +95,9 @@ def index():
                 os.remove(file_path)
         subject = request.form.get('subject')
         html_content = request.form.get('html_content')
+        return redirect(url_for('index'))
 
-    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page', per_page=10)
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page', per_page=50)
     total = len(email_list)
     pagination_emails = email_list[offset:offset + per_page]
     pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
@@ -96,6 +108,17 @@ def index():
 def send_emails():
     thread = threading.Thread(target=send_bulk_emails)
     thread.start()
+    return redirect(url_for('index'))
+
+@app.route('/clear_emails')
+def clear_emails():
+    global email_list, progress
+    email_list = []
+    progress = {
+        'total': 0,
+        'sent': 0,
+        'failed': 0
+    }
     return redirect(url_for('index'))
 
 @app.route('/progress')
